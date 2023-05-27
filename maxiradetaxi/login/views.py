3c22c04a308901592653699f00cf66b568dcef2f
max <mvlmvl25@gmail.com>
2023-05-28 00:03:04 +0300

    login interface

--- maxiradetaxi/login/views.py
+++ maxiradetaxi/login/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login(request):
+    #return HttpResponse("<h4>Hello world</h4>")
+    return render(request, 'login/login.html')
+
--- maxiradetaxi/maxiradetaxi/settings.py
+++ maxiradetaxi/maxiradetaxi/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-_m6p2!598^@@q=uq6fo*jruu810s(%-ws7#i%%m!xh++87tlbk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.103']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
